5a5c696063639ab337513a0844c7019100381697
bora1102 <44738503+bora1102@users.noreply.github.com>
2018-11-28 14:24:59 +0900

    Add DTO

---
+++ University_cms/src/main/java/com/university/cmsDTO/ProfessorDTO.java
@@ -0,0 +1,78 @@
+package com.university.cmsDTO;
+
+public class ProfessorDTO {
+	//교수
+	private int pNo; //교수 고유번호
+	private String pNum; //교수번호
+	private String pPw; //교수 비밀번호
+	private String pName; //교수 이름
+	private String pMajor; //소속대학
+	private String pRRN; //교수 주민번호
+	private String pTel; //교수 전화번호
+	private String pEmail; //교수 이메일
+	private String pAddr; //교수 주소
+	private String pStatus; //교수 상태
+	
+	public int getpNo() {
+		return pNo;
+	}
+	public void setpNo(int pNo) {
+		this.pNo = pNo;
+	}
+	public String getpNum() {
+		return pNum;
+	}
+	public void setpNum(String pNum) {
+		this.pNum = pNum;
+	}
+	public String getpPw() {
+		return pPw;
+	}
+	public void setpPw(String pPw) {
+		this.pPw = pPw;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getpMajor() {
+		return pMajor;
+	}
+	public void setpMajor(String pMajor) {
+		this.pMajor = pMajor;
+	}
+	public String getpRRN() {
+		return pRRN;
+	}
+	public void setpRRN(String pRRN) {
+		this.pRRN = pRRN;
+	}
+	public String getpTel() {
+		return pTel;
+	}
+	public void setpTel(String pTel) {
+		this.pTel = pTel;
+	}
+	public String getpEmail() {
+		return pEmail;
+	}
+	public void setpEmail(String pEmail) {
+		this.pEmail = pEmail;
+	}
+	public String getpAddr() {
+		return pAddr;
+	}
+	public void setpAddr(String pAddr) {
+		this.pAddr = pAddr;
+	}
+	public String getpStatus() {
+		return pStatus;
+	}
+	public void setpStatus(String pStatus) {
+		this.pStatus = pStatus;
+	}
+	
+	
+}
--- University_cms/src/main/java/com/university/cmsDTO/StudentDTO.java
+++ University_cms/src/main/java/com/university/cmsDTO/StudentDTO.java
@@ -1,6 +1,77 @@
 package com.university.cmsDTO;
 
 public class StudentDTO {
+	//학생
+	private int sNo; //학생 고유번호
+	private String sNum; //학번
+	private String sPw; //학생 비밀번호
+	private String sName; //학생 이름
+	private String sMajor; //학생 전공
+	private String sRRN; //학생 주민번호
+	private String sTel; //학생 전화번호
+	private String sEmail; //학생 이메일
+	private String sAddr; //학생 주소
+	private String sStatus; //학생 상태
 	
+	public int getsNo() {
+		return sNo;
+	}
+	public void setsNo(int sNo) {
+		this.sNo = sNo;
+	}
+	public String getsNum() {
+		return sNum;
+	}
+	public void setsNum(String sNum) {
+		this.sNum = sNum;
+	}
+	public String getsPw() {
+		return sPw;
+	}
+	public void setsPw(String sPw) {
+		this.sPw = sPw;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsRRN() {
+		return sRRN;
+	}
+	public void setsRRN(String sRRN) {
+		this.sRRN = sRRN;
+	}
+	public String getsTel() {
+		return sTel;
+	}
+	public void setsTel(String sTel) {
+		this.sTel = sTel;
+	}
+	public String getsEmail() {
+		return sEmail;
+	}
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+	public String getsAddr() {
+		return sAddr;
+	}
+	public void setsAddr(String sAddr) {
+		this.sAddr = sAddr;
+	}
+	public String getsStatus() {
+		return sStatus;
+	}
+	public void setsStatus(String sStatus) {
+		this.sStatus = sStatus;
+	}
+	public String getsMajor() {
+		return sMajor;
+	}
+	public void setsMajor(String sMajor) {
+		this.sMajor = sMajor;
+	}
 	
 }
